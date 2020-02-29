a754b7e2003ff34ec56c629049a65099da195631
cholodymedia <cholodymedia@gmail.com>
2020-02-29 20:02:27 +0100

    Create router views

---
+++ src/views/Blog.vue
@@ -0,0 +1,32 @@
+<template>
+  <div class="wrapper">Blog</div>
+</template>
+
+<script>
+export default {
+  name: 'Blog'
+}
+</script>
+
+<style lang="scss" scoped>
+@import url('https://fonts.googleapis.com/css?family=Montserrat:300,400,500,600&display=swap');
+* {
+  margin: 0;
+  padding: 0;
+  -webkit-tap-highlight-color: transparent;
+  scroll-behavior: smooth;
+}
+.wrapper {
+  font-family: 'Montserrat';
+  width: 100vw;
+  height: 100vh;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  font-size: 10rem;
+  background-color: #242736;
+  color: #68caa3;
+  font-weight: bold;
+}
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,54 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div id="app">
+    <Start/>
+    <Projects/>
+    <About/>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+import Start from '@/components/Start.vue';
+import Projects from '@/components/Projects.vue';
+import About from '@/components/About.vue';
 
 export default {
-  name: 'Home',
+  name: 'app',
   components: {
-    HelloWorld
-  }
-}
+    Start,
+    Projects,
+    About,
+  },
+};
 </script>
+
+<style lang="scss">
+@import url('https://fonts.googleapis.com/css?family=Montserrat:300,400,500,600&display=swap');
+* {
+  margin: 0;
+  padding: 0;
+  -webkit-tap-highlight-color: transparent;
+  scroll-behavior: smooth;
+}
+#app {
+  font-family: 'Montserrat';
+  width: 100vw;
+  height: auto;
+  display: flex;
+  flex-direction: column;
+  overflow-x: hidden;
+}
+body {
+  overflow-x: hidden;
+}
+::-webkit-scrollbar {
+  width: 12px;
+  background: #212121;
+}
+::-webkit-scrollbar-thumb {
+  background: #2D98B8;
+  border-radius: 10px;
+}
+::-webkit-scrollbar-thumb:hover {
+  background: #2D98B8;
+}
+</style>
