bacf42690e6b3266934b160a251c5eea5c9c4bc0
cholodymedia <cholodymedia@gmail.com>
2020-04-17 20:28:58 +0200

    Delete Admin

--- src/router/index.js
+++ src/router/index.js
@@ -3,9 +3,6 @@ import VueRouter from 'vue-router'
 import Home from '../views/Home.vue'
 import Blog from '../views/Blog.vue'
 import Err from '../views/Error.vue'
-import Admin from '../views/Admin.vue'
-import Dashboard from '../views/Dashboard.vue'
-import axios from 'axios'
 
 Vue.use(VueRouter)
 
@@ -24,18 +21,6 @@ const routes = [
     path: '/404',
     name: 'Error',
     component: Err
-  },
-  {
-    path: '/admin',
-    name: 'Admin',
-    component: Admin,
-    meta: {onlylogout: true}
-  },
-  {
-    path: '/dashboard',
-    name: 'Dashboard',
-    component: Dashboard,
-    meta: {requiresAuth: true}
   }
 ]
 
@@ -46,27 +31,8 @@ const router = new VueRouter({
 })
 
 router.beforeEach((to, from, next) => {
-  const requiresAuth = to.matched.some(record => record.meta.requiresAuth);
-  const onlylogout = to.matched.some(record => record.meta.onlylogout);
-  const token = localStorage.getItem("token");
-  if(to.name == 'Home' || to.name == 'Blog' || to.name == 'Error' || to.name == 'Admin' || to.name == 'Dashboard') {
-    if(requiresAuth) {
-      axios.post('https://cholodymedia-analytics.herokuapp.com/checktoken', {
-        token
-      }).then(() => {
-        next();
-      }).catch(() => {
-        localStorage.removeItem("token")
-        next('/admin')
-      })
-    }else {
-      next();
-    }
-    if(onlylogout) {
-      if(token) {
-        next('/dashboard');
-      }
-    }
+  if(to.name == 'Home' || to.name == 'Blog' || to.name == 'Error') {
+    next();
   } else {
     next('/404');
   }
