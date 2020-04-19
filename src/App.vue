ce507e964ca27c9e4006a240833015accb93f42c
cholodymedia <cholodymedia@gmail.com>
2020-04-19 23:49:16 +0200

    Remove axios request

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,3 @@
 <template>
   <router-view/>
 </template>
-
-<script>
-import axios from 'axios'
-
-export default {
-  created() {
-    if(!localStorage.getItem("visited")) {
-      localStorage.setItem('visited', "yes");
-      axios.post('https://cholodymedia-analytics.herokuapp.com/site/open');
-    }
-  }
-}
-</script>
